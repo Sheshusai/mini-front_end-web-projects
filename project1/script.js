477dc12723151a00539b51a53f83c7373309a9dd
Sheshusai <sheshumadhunala1988@gmail.com>
2023-05-24 20:13:00 +0530

    first project

---
+++ project1/script.js
@@ -0,0 +1,22 @@
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterableCards = document.querySelectorAll(".filterable_cards .card");
+
+const filtercards = (e) =>{
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+
+        card.classList.add("hide");
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "show all"){
+            card.classList.remove("hide");
+        }
+    })
+
+
+}
+
+
+
+
+filterButtons.forEach(button => button.addEventListener("click",filtercards));
